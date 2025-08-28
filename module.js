d2738ed51d9bc2821302475d0fad604502bb6e1c
RenatoDev450 <renatooliveiramendes27@gmail.com>
2025-08-28 01:26:45 +0000

    jjm

---
+++ module.js
@@ -0,0 +1,16 @@
+/* var input = require('readline-sync');
+
+let numerador = input.questionInt("digite um numerador")
+let denominador = input.questionInt("digite um denominador")
+
+let quociente = numerador/denominador; 
+
+
+let resto =Math.ceil((quociente - parseInt(quociente))*denominador)
+console.log(resto, "ou", numerador%denominador);
+ */
+let n1 = 5
+let n2 = 3
+let n3 = 2 
+
+if (3<5) {console.log(n1,n2,n3)}
